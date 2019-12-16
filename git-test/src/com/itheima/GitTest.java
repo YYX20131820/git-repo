e4d2fa3b0fc1dd5ea3572eaa9dfb0458e8b4e8cd
yangyuxiang <1715868775@qq.com>
2019-12-16 12:12:11 +0800

    将本地工程添加到git

--- git-test/src/com/itheima/GitTest.java
+++ git-test/src/com/itheima/GitTest.java
@@ -4,6 +4,11 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试git");
     }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+    }
 }
 /**
  * @Author YangYuxiang
